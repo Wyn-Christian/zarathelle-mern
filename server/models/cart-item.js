1e078a7b67ac43986c63f416ea0e495582c6fbd5
LJ <velasquezlovenjoy@gmail.com>
2023-05-18 10:17:53 +0800

    create  cart-item model

---
+++ server/models/cart-item.js
@@ -0,0 +1,27 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+const CartItemSchema = new Schema(
+  {
+    user: {
+      type: mongoose.Types.ObjectId,
+      required: true,
+      ref: "user",
+    },
+    product: {
+      type: mongoose.Types.ObjectId,
+      required: true,
+      ref: "product",
+    },
+    quantity: {
+      type: String,
+      default: 1,
+    },
+    image: String,
+  },
+  {
+    timestamps: true,
+  }
+);
+
+module.exports = mongoose.model("cart-item", CartItemSchema);
