cf1774bc689ab8c0c24ab6e86d500f42528aa6f8
LJ <velasquezlovenjoy@gmail.com>
2023-05-18 10:18:14 +0800

    create order model

---
+++ server/models/order.js
@@ -0,0 +1,38 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+const OrderSchema = new Schema(
+  {
+    user: {
+      type: mongoose.Types.ObjectId,
+      required: true,
+      ref: "user",
+    },
+    items: [
+      {
+        type: mongoose.Types.ObjectId,
+        required: true,
+        ref: "order-item",
+      },
+    ],
+    total_quantity: {
+      type: Number,
+      required: true,
+      default: 1,
+    },
+    total_price: {
+      type: mongoose.Types.Decimal128,
+      required: true,
+    },
+    status: {
+      type: String,
+      required: true,
+      enum: ["in progress", "on its way", "delivered"],
+      default: "in progress",
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+module.exports = mongoose.model("order", OrderSchema);
