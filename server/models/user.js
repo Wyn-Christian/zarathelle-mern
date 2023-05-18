b4e2cea1cc4f55eba64f07a04d47c85d54270f01
LJ <velasquezlovenjoy@gmail.com>
2023-05-18 10:18:33 +0800

    create user model

--- server/models/user.js
+++ server/models/user.js
@@ -1,9 +1,41 @@
 const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
-const UserSchema = new Schema({
-  name: {
-    type: String,
-    required: true,
+const UserSchema = new Schema(
+  {
+    username: {
+      type: String,
+      required: true,
+    },
+    first_name: {
+      type: String,
+      required: true,
+    },
+    last_name: {
+      type: String,
+      required: true,
+    },
+    address: String,
+    phone: String,
+    email: {
+      type: String,
+      required: true,
+    },
+    password: {
+      type: String,
+      required: true,
+    },
+    image: String,
+    position: {
+      type: String,
+      required: true,
+      enum: ["user", "admin"],
+      default: "user",
+    },
   },
-});
+  {
+    timestamps: true,
+  }
+);
+
+module.exports = mongoose.model("user", UserSchema);
