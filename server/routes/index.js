9a2027dd099b4b25b87641d8be8a6f490967691b
LJ <velasquezlovenjoy@gmail.com>
2023-05-18 10:28:56 +0800

    add multer in index routes

--- server/routes/index.js
+++ server/routes/index.js
@@ -1,9 +1,27 @@
-var express = require('express');
+var express = require("express");
 var router = express.Router();
+const multer = require("multer");
+
+var storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    cb(null, "./public/images");
+  },
+  filename: function (req, file, cb) {
+    const uniqueSuffix = Date.now() + "-" + Math.round(Math.random() * 1e9);
+    cb(
+      null,
+      uniqueSuffix + "-" + file.originalname.replace(/\s+/g, "-").toLowerCase()
+    );
+  },
+});
+
+const upload = multer({
+  storage: storage,
+});
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get("/", function (req, res, next) {
+  res.json({ message: "This is the index route of the server" });
 });
 
 module.exports = router;
