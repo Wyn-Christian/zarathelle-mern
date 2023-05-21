e88d58dd61b8a6f20130bdf93a028469d7b97620
Wyn Chrsitian <wynchristian.rebanal@tup.edu.ph>
2023-05-21 22:59:18 +0800

    Create Admin Root

---
+++ client/src/routes/admin/AdminRoot.jsx
@@ -0,0 +1,26 @@
+import { Box, Toolbar } from "@mui/material";
+import { Outlet } from "react-router-dom";
+import AdminNavBar from "../../components/AdminNavbar";
+
+const drawerWidth = 240;
+
+function AdminRoot() {
+  return (
+    <Box display="flex">
+      <AdminNavBar />
+      <Box
+        component="main"
+        sx={{
+          flexGrow: 1,
+          p: 3,
+          width: { sm: `calc(100% - ${drawerWidth}px)` },
+        }}
+      >
+        <Toolbar />
+        <Outlet />
+      </Box>
+    </Box>
+  );
+}
+
+export default AdminRoot;
