68dd761e988dba370dfb17084c9486074d872e3b
Wyn Chrsitian <wynchristian.rebanal@tup.edu.ph>
2023-05-21 11:56:18 +0800

    Add Routes, Redux Store Provider and Theme Provider

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,10 +1,67 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import { Provider } from "react-redux";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+import theme from "./app/theme.jsx";
+import store from "./app/store.js";
+
+// Components routes
+import ErrorPage from "./routes/ErrorPage.jsx";
+import Root from "./routes/Root.jsx";
+import { ThemeProvider } from "@mui/material";
+import Home from "./routes/client/Home.jsx";
+import Collections from "./routes/client/Collections.jsx";
+import Login from "./routes/client/Login.jsx";
+import SignUp from "./routes/client/SignUp.jsx";
+import ProductList from "./routes/client/ProductList.jsx";
+import Product from "./routes/client/Product.jsx";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Root />,
+    errorElement: <ErrorPage />,
+    children: [
+      {
+        errorElement: <ErrorPage />,
+        children: [
+          {
+            index: true,
+            element: <Home />,
+          },
+          {
+            path: "/collections",
+            element: <Collections />,
+          },
+          {
+            path: "/collections/:id",
+            element: <ProductList />,
+          },
+          {
+            path: "/product/:id",
+            element: <Product />,
+          },
+          {
+            path: "/login",
+            element: <Login />,
+          },
+          {
+            path: "/sign-up",
+            element: <SignUp />,
+          },
+        ],
+      },
+    ],
+  },
+]);
+
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+    <ThemeProvider theme={theme}>
+      <Provider store={store}>
+        <RouterProvider router={router} />
+      </Provider>
+    </ThemeProvider>
+  </React.StrictMode>
+);
