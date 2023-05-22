696b73978ecbced865c9118f4779e8ed14b4358e
Wyn Chrsitian <wynchristian.rebanal@tup.edu.ph>
2023-05-22 16:01:35 +0800

    Add style for medium desktop

--- client/src/routes/admin/AdminRoot.jsx
+++ client/src/routes/admin/AdminRoot.jsx
@@ -13,7 +13,7 @@ function AdminRoot() {
         sx={{
           flexGrow: 1,
           p: 3,
-          width: { sm: `calc(100% - ${drawerWidth}px)` },
+          width: { xs: "100%", sm: `calc(100% - ${drawerWidth}px)` },
         }}
       >
         <Toolbar />
