53cc514a7b481fe1a94b849e3368464b1b000133
Wyn Chrsitian <wynchristian.rebanal@tup.edu.ph>
2023-05-23 14:48:26 +0800

    Modify Collection services

--- server/services/collection.js
+++ server/services/collection.js
@@ -1,22 +1,31 @@
 const Collection = require("../models/collection");
-const Product = require("../models/product");
+const { Product } = require("../models/product");
 
 exports.list = (req, res, next) => {
   Collection.find()
     .then((result) => res.json(result))
-    .catch((err) => next(err));
+    .catch((err) => {
+      console.log(err);
+      next(err);
+    });
 };
 
 exports.detail = (req, res, next) => {
   Collection.findById(req.params.id)
     .then((result) => res.json(result))
-    .catch((err) => next(err));
+    .catch((err) => {
+      console.log(err);
+      next(err);
+    });
 };
 
 exports.list_product_by_collection = (req, res, next) => {
-  Product.find({ collection: req.params.id })
+  Product.find({ collection_id: req.params.id })
     .then((result) => res.json(result))
-    .catch((err) => next(err));
+    .catch((err) => {
+      console.log(err);
+      next(err);
+    });
 };
 
 exports.create = (req, res, next) => {
@@ -25,33 +34,47 @@ exports.create = (req, res, next) => {
   const new_collection = new Collection({
     title,
     description,
-    image,
+    image: req.file.filename,
     category,
   });
 
   new_collection
     .save()
     .then((result) => res.json(result))
-    .catch((err) => next(err));
+    .catch((err) => {
+      console.log(err);
+      next(err);
+    });
 };
 
 exports.update = (req, res, next) => {
-  const { title, description, image, category } = req.body;
+  const { title, description } = req.body;
 
   const collection = {
     title,
     description,
-    image,
-    category,
   };
+
+  if (req.body.category) {
+    collection.category = req.body.category;
+  }
+  if (req.file) {
+    collection.image = req.file.filename;
+  }
 
   Collection.findByIdAndUpdate(req.params.id, collection, { new: true })
     .then((result) => res.json(result))
-    .catch((err) => next(err));
+    .catch((err) => {
+      console.log(err);
+      next(err);
+    });
 };
 
 exports.delete = (req, res, next) => {
   Collection.findByIdAndDelete(req.params.id)
     .then((result) => res.json(result))
-    .catch((err) => next(err));
+    .catch((err) => {
+      console.log(err);
+      next(err);
+    });
 };
