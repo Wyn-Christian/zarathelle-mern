f9a9e39f416e931b324f58e88b312d9df6926296
Wyn Chrsitian <wynchristian.rebanal@tup.edu.ph>
2023-05-23 14:48:50 +0800

    Modify Product services

--- server/services/product.js
+++ server/services/product.js
@@ -2,47 +2,71 @@ const { Product } = require("../models/product");
 
 exports.list = (req, res, next) => {
   Product.find()
-    .populate("collection")
+    .populate("collection_id")
     .then((result) => res.json(result))
-    .catch((err) => next(err));
+    .catch((err) => {
+      console.log(err);
+      next(err);
+    });
 };
 
 exports.detail = (req, res, next) => {
   Product.findById(req.params.id)
-    .populate("collection")
+    .populate("collection_id")
     .then((result) => res.json(result))
-    .catch((err) => next(err));
+    .catch((err) => {
+      console.log(err);
+      next(err);
+    });
 };
 
 exports.create = (req, res, next) => {
-  const { name, description, image, category, collection } = req.body;
+  const { name, description, category, collection_id, stocks } = req.body;
 
   const new_product = new Product({
     name,
     description,
-    image,
+    image: req.file.filename,
     category,
-    collection,
+    collection_id,
     stocks,
   });
 
   new_product
     .save()
     .then((result) => res.json(result))
-    .catch((err) => next(err));
+    .catch((err) => {
+      console.log(err);
+      next(err);
+    });
 };
 
 exports.update = (req, res, next) => {
-  const product = ({ name, description, image, category, collection, stocks } =
-    req.body);
+  const product = ({ name, description, stocks } = req.body);
 
+  if (req.body.collection_id) {
+    product.collection_id = req.body.collection_id;
+  }
+  if (req.body.category) {
+    product.category = req.body.category;
+  }
+
+  if (req.file) {
+    product.image = req.file.filename;
+  }
   Product.findByIdAndUpdate(req.params.id, product, { new: true })
     .then((result) => res.json(result))
-    .catch((err) => next(err));
+    .catch((err) => {
+      console.log(err);
+      next(err);
+    });
 };
 
 exports.delete = (req, res, next) => {
   Product.findByIdAndDelete(req.params.id)
     .then((result) => res.json(result))
-    .catch((err) => next(err));
+    .catch((err) => {
+      console.log(err);
+      next(err);
+    });
 };
