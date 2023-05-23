fd3f103c1637d974bb73bc8bd7227b077216f8be
Wyn Chrsitian <wynchristian.rebanal@tup.edu.ph>
2023-05-23 22:29:16 +0800

    Add reducers in root reducer

--- client/src/app/store.js
+++ client/src/app/store.js
@@ -1,5 +1,13 @@
 import { configureStore } from "@reduxjs/toolkit";
+import { apiSlice } from "../features/apiSlice";
+
+import userReducer from "../features/usersSlice";
 
 export default configureStore({
-  reducer: {},
+  reducer: {
+    user: userReducer,
+    [apiSlice.reducerPath]: apiSlice.reducer,
+  },
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware().concat(apiSlice.middleware),
 });
