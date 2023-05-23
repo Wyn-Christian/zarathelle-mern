b74bf2344f87168d94dd57a8f89477f46a2ad6e9
Wyn Chrsitian <wynchristian.rebanal@tup.edu.ph>
2023-05-23 14:48:10 +0800

    Modify order services

--- server/services/order.js
+++ server/services/order.js
@@ -1,57 +1,96 @@
 const Order = require("../models/order");
 const OrderItem = require("../models/order-item");
 const CartItem = require("../models/cart-item");
+const { Product } = require("../models/product");
 
 exports.list = (req, res, next) => {
   Order.find()
-    .populate("product")
+    .populate("items")
     .then((result) => res.json(result))
-    .catch((err) => next(err));
+    .catch((err) => {
+      console.log(err);
+      next(err);
+    });
 };
 
 exports.detail = (req, res, next) => {
   Order.findById(req.params.id)
-    .populate("product")
+    .populate("items")
     .then((result) => res.json(result))
-    .catch((err) => next(err));
+    .catch((err) => {
+      console.log(err);
+      next(err);
+    });
 };
 
 exports.list_user = (req, res, next) => {
   Order.find({ user: req.params.user_id })
+    .populate("items")
     .then((result) => res.json(result))
-    .catch((err) => next(err));
+    .catch((err) => {
+      console.log(err);
+      next(err);
+    });
 };
 
 exports.checkout = (req, res, next) => {
   const new_order = new Order({
-    user: req.body.user_id,
+    user: req.body.user,
     items: req.body.items,
     total_quantity: req.body.total_quantity,
     total_price: req.body.total_price,
   });
 
-  new_order.save().then(async (result) => {
-    let order_items_result = await OrderItem.insertMany(req.body.items);
-    console.log(
-      `${order_items_result.length} checkout items documents were inserted`
-    );
-    // let cart_delete_result = await CartItem.deleteMany({
-    //   user: req.body.user_id,
-    // });
-    // console.log(`Deleted ${cart_delete_result.deletedCount} documents`);
-    res.json(result);
-  });
+  new_order
+    .save()
+    .then(async (result) => {
+      let order_items_result = await OrderItem.insertMany(req.body.items);
+      console.log(
+        `${order_items_result.length} checkout items documents were inserted`
+      );
+      let cart_delete_result = await CartItem.deleteMany({
+        user: req.body.user,
+      });
+      console.log(`Deleted ${cart_delete_result.deletedCount} documents`);
+      res.json(result);
+    })
+    .catch((err) => {
+      console.log(err);
+      next(err);
+    });
 };
 
 exports.status_change = (req, res, next) => {
-  if (req.body.status === "delivered") {
-    // update the stocks and num_sold of the product
-  }
   Order.findByIdAndUpdate(
     req.params.id,
     { status: req.body.status },
     { new: true }
   )
-    .then((result) => res.json(result))
-    .catch((err) => next(err));
+    .populate("items")
+    .then((result) => {
+      if (req.body.status === "delivered") {
+        // update the stocks and num_sold of the product
+        result.items.forEach((item) => {
+          Product.findByIdAndUpdate(
+            item.product,
+            {
+              $inc: {
+                num_sold: item.quantity,
+                stocks: -item.quantity,
+              },
+            },
+            { new: true }
+          ).then((result) =>
+            console.log(
+              `Product ${result.name} stock and num_sold updated`
+            )
+          );
+        });
+      }
+      res.json(result);
+    })
+    .catch((err) => {
+      console.log(err);
+      next(err);
+    });
 };
