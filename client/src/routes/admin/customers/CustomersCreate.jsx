0313cb49d55f26037c94b6df3dd0fa4b47fb56a6
Wyn Chrsitian <wynchristian.rebanal@tup.edu.ph>
2023-05-24 15:00:03 +0800

    Add profile image upload

--- client/src/routes/admin/customers/CustomersCreate.jsx
+++ client/src/routes/admin/customers/CustomersCreate.jsx
@@ -1,7 +1,48 @@
 import Grid from "@mui/material/Unstable_Grid2";
+import { MuiFileInput } from "mui-file-input";
 import { Box, Button, Paper, TextField, Typography } from "@mui/material";
+import { useNavigate } from "react-router-dom";
+import { useSignUpUserMutation } from "../../../features/apiSlice";
+import { useFormik } from "formik";
 
 function CustomersCreate() {
+  const navigate = useNavigate();
+  const [signUpUser] = useSignUpUserMutation();
+
+  const formik = useFormik({
+    initialValues: {
+      username: "",
+      first_name: "",
+      last_name: "",
+      address: "",
+      phone: "",
+      email: "",
+      password: "",
+      repassword: "",
+      image: null,
+    },
+  });
+
+  const onSubmit = async () => {
+    const new_user = new FormData();
+    new_user.append("username", formik.values.username);
+    new_user.append("first_name", formik.values.first_name);
+    new_user.append("last_name", formik.values.last_name);
+    new_user.append("address", formik.values.address);
+    new_user.append("phone", formik.values.phone);
+    new_user.append("email", formik.values.email);
+    new_user.append("password", formik.values.password);
+    new_user.append("image", formik.values.image);
+
+    await signUpUser(new_user)
+      .unwrap()
+      .then((res) => {
+        console.log("Create User Successfully", res);
+        navigate(`/admin/customers`);
+      })
+      .catch((err) => console.error(err));
+  };
+
   return (
     <Box>
       <Typography variant="h4">Create Customer</Typography>
@@ -37,8 +78,15 @@ function CustomersCreate() {
                 fullWidth
               />
             </Grid>
-            <Grid xs={12}>
+            <Grid xs={12} md={6}>
               <TextField label="Phone Number" name="phone" fullWidth />
+            </Grid>
+            <Grid xs={12} md={6}>
+              <MuiFileInput
+                value={formik.values.image}
+                onChange={(e) => formik.setFieldValue("image", e)}
+                placeholder="Profile Image"
+              />
             </Grid>
             <Grid xs={12}>
               <TextField
@@ -54,6 +102,7 @@ function CustomersCreate() {
                 type="submit"
                 variant="contained"
                 sx={{ width: { xs: "100%", md: 160 } }}
+                onClick={onSubmit}
               >
                 Add Customer
               </Button>
