07344d10ad7bb0d5a12ca10580a8bbfa5a1f442a
Wyn Chrsitian <wynchristian.rebanal@tup.edu.ph>
2023-05-24 15:03:19 +0800

    Data fetching in Orders

--- client/src/routes/client/Orders.jsx
+++ client/src/routes/client/Orders.jsx
@@ -1,3 +1,5 @@
+import { useSelector } from "react-redux";
+import { userSelector } from "../../features/usersSlice";
 import {
   Box,
   Button,
@@ -10,8 +12,18 @@ import {
 } from "@mui/material";
 import Footer from "../../components/Footer";
 import { useTheme } from "@mui/material/styles";
+import CircularProgress from "@mui/material/CircularProgress";
 
-const OrderItem = () => (
+import {
+  useChangeStatusOrderMutation,
+  useGetOrdersByUserQuery,
+} from "../../features/apiSlice";
+import { PHPPrice } from "../../app/priceFormatter";
+import { useEffect } from "react";
+import { api_base_url } from "../../app/base_url";
+import { enqueueSnackbar } from "notistack";
+
+const OrderItem = ({ product, quantity, price, image_url }) => (
   <Box sx={{ display: "flex", mb: 3 }}>
     <Paper>
       <CardMedia
@@ -19,7 +31,11 @@ const OrderItem = () => (
           width: { xs: 125, md: 90 },
           height: { xs: 125, md: 90 },
         }}
-        image="/images/sample/product.jpg"
+        image={
+          product?.category === "ready made"
+            ? `${api_base_url}${product?.image_url}`
+            : `${api_base_url}${image_url}`
+        }
       />
     </Paper>
     <Box
@@ -31,11 +47,12 @@ const OrderItem = () => (
       }}
     >
       <Box sx={{ display: "flex", flexDirection: "column", flexGrow: 2 }}>
-        <Typography fontWeight="bold" sx={{ maxWidth: 375 }}>
-          Product Title Product Title Product Title Product Title Product
-          Title Product Title
+        <Typography fontWeight="bold" sx={{ width: { md: 375 } }}>
+          {product?.name}
         </Typography>
-        <Typography>Type</Typography>
+        <Typography variant="caption">
+          {product?.category.toUpperCase()}
+        </Typography>
       </Box>
       <Box
         sx={{
@@ -44,15 +61,52 @@ const OrderItem = () => (
           justifyContent: "space-between",
         }}
       >
-        <Typography>Qty: 3</Typography>
-        <Typography>Price: 300.00</Typography>
+        <Typography>Qty: {quantity}</Typography>
+        <Typography>Price: {PHPPrice.format(price)}</Typography>
       </Box>
     </Box>
   </Box>
 );
 
-const OrderItems = () => {
+const StatusInfo = ({ status }) => {
+  let content;
+  switch (status) {
+    case "to process":
+      content = <Chip label="To Process" color="warning" />;
+      break;
+    case "on its way":
+      content = <Chip label="On Its Way" color="info" />;
+      break;
+    case "delivered":
+      content = <Chip label="Delivered" color="success" />;
+      break;
+    case "cancelled":
+      content = <Chip label="Cancelled" color="error" />;
+      break;
+  }
+  return content;
+};
+
+const OrderItems = ({
+  items = [],
+  id,
+  status,
+  total_quantity,
+  total_price,
+}) => {
   const theme = useTheme();
+  const [changeStatusOrder] = useChangeStatusOrderMutation();
+
+  const handleCancelClick = async () => {
+    await changeStatusOrder({ id, data: { status: "cancelled" } })
+      .unwrap()
+      .then((res) =>
+        enqueueSnackbar("Order Cancelled Successfully!", {
+          variant: "success",
+        })
+      );
+  };
+
   return (
     <Paper
       elevation={3}
@@ -67,26 +121,76 @@ const OrderItems = () => {
         }}
       >
         <Box>
-          <Chip color="primary" label="To Process" />
+          <StatusInfo status={status} />
         </Box>
-        <Button sx={{ color: theme.palette.secondary.main }}>
-          Cancel
-        </Button>
+        {status === "to process" && (
+          <Button
+            sx={{ color: theme.palette.secondary.main }}
+            onClick={handleCancelClick}
+          >
+            Cancel
+          </Button>
+        )}
       </Box>
       <Divider />
       <Box sx={{ p: "10px 20px" }}>
-        <OrderItem />
-        <OrderItem />
+        {items.map((item) => (
+          <OrderItem key={item.id} {...item} />
+        ))}
       </Box>
       <Divider />
       <Box sx={{ p: 2 }}>
-        <Typography textAlign="end">Total(3 items): 1,200.00</Typography>
+        <Typography textAlign="end">
+          Total({total_quantity} items): {PHPPrice.format(total_price)}
+        </Typography>
       </Box>
     </Paper>
   );
 };
 
 function Orders() {
+  const user = useSelector(userSelector);
+  const {
+    data: orders = [],
+    isSuccess,
+    isLoading,
+  } = useGetOrdersByUserQuery(user.id);
+
+  let content;
+  if (isLoading) {
+    content = (
+      <Box
+        sx={{
+          display: "flex",
+          justifyContent: "center",
+          alignItems: "center",
+          height: "40vh",
+        }}
+      >
+        <CircularProgress size={150} />
+      </Box>
+    );
+  } else if (isSuccess) {
+    content = (
+      <Box>
+        <Typography variant="h2" textAlign="center" sx={{ m: 4 }}>
+          Orders
+        </Typography>
+        <Box
+          sx={{
+            display: "flex",
+            flexDirection: "column",
+            alignItems: "center",
+          }}
+        >
+          {orders.map((order) => (
+            <OrderItems key={order.id} {...order} />
+          ))}
+        </Box>
+      </Box>
+    );
+  }
+
   return (
     <Box>
       <Box
@@ -110,21 +214,7 @@ function Orders() {
           },
         }}
       />
-      <Container sx={{ minHeight: "10vh" }}>
-        <Typography variant="h2" textAlign="center" sx={{ m: 4 }}>
-          Orders
-        </Typography>
-        <Box
-          sx={{
-            display: "flex",
-            flexDirection: "column",
-            alignItems: "center",
-          }}
-        >
-          <OrderItems />
-          <OrderItems />
-        </Box>
-      </Container>
+      <Container sx={{ minHeight: "10vh" }}>{content}</Container>
       <Footer />
     </Box>
   );
