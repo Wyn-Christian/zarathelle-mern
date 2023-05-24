5b38790684ac59799101f934cba339bf81c7ea73
Wyn Chrsitian <wynchristian.rebanal@tup.edu.ph>
2023-05-24 15:03:36 +0800

    Data fetching in cart items

--- client/src/routes/client/CartItems.jsx
+++ client/src/routes/client/CartItems.jsx
@@ -6,6 +6,7 @@ import {
   Container,
   Divider,
   Paper,
+  Stack,
   Typography,
 } from "@mui/material";
 import Footer from "../../components/Footer";
@@ -13,84 +14,222 @@ import Footer from "../../components/Footer";
 import RemoveIcon from "@mui/icons-material/Remove";
 import AddIcon from "@mui/icons-material/Add";
 import DeleteIcon from "@mui/icons-material/Delete";
+import { useSelector } from "react-redux";
+import { userSelector } from "../../features/usersSlice";
+import {
+  useCheckoutOrderMutation,
+  useGetCartByUserQuery,
+  useRemoveToCartMutation,
+  useUpdateCartMutation,
+} from "../../features/apiSlice";
+import CircularProgress from "@mui/material/CircularProgress";
+import { api_base_url } from "../../app/base_url";
+import { PHPPrice } from "../../app/priceFormatter";
+import { enqueueSnackbar } from "notistack";
+import { useNavigate } from "react-router-dom";
 
-const CartItem = ({ title, quantity, type, image }) => (
-  <Box
-    sx={{
-      display: "flex",
-      flexDirection: { xs: "column", md: "row" },
+const CartItem = ({ i, id, quantity, image_url, product }) => {
+  const [updateCart] = useUpdateCartMutation();
+  const [removeToCart] = useRemoveToCartMutation();
 
-      mb: 3,
-    }}
-  >
+  const increaseQuantity = async () => {
+    if (quantity + 1 < product.stocks) {
+      let data = { id, data: { quantity: quantity + 1 } };
+      await updateCart(data).unwrap();
+    }
+  };
+  const decreaseQuantity = async () => {
+    if (quantity - 1 !== 0) {
+      let data = { id, data: { quantity: quantity - 1 } };
+      await updateCart(data).unwrap();
+    }
+  };
+
+  const deleteCart = async () => {
+    await removeToCart(id).then((res) => {
+      enqueueSnackbar("Cart Item Deleted", { variant: "success" });
+    });
+  };
+
+  return (
     <Box
       sx={{
         display: "flex",
-        mb: { xs: 3, md: 0 },
+        flexDirection: { xs: "column", md: "row" },
       }}
     >
-      <Typography variant="h4" sx={{ alignSelf: "center", mr: 3 }}>
-        1
-      </Typography>
-      <Paper>
-        <CardMedia
-          sx={{
-            width: { xs: 150, md: 100 },
-            height: { xs: 150, md: 100 },
-          }}
-          image="/images/sample/product.jpg"
-        />
-      </Paper>
       <Box
         sx={{
-          ml: 3,
           display: "flex",
-          flexDirection: "column",
+          mb: { xs: 3, md: 0 },
         }}
       >
-        <Box sx={{ maxWidth: { xs: 350, md: 500 } }}>
-          <Typography fontWeight="bold">
-            Product Title Product Title Product Title Product Title
-          </Typography>
-        </Box>
-        <Box>
-          <Typography>Type</Typography>
-        </Box>
-        <Box>
-          <Typography>100.00</Typography>
+        <Typography variant="h4" sx={{ alignSelf: "center", mr: 3 }}>
+          {i + 1}
+        </Typography>
+        <Paper>
+          <CardMedia
+            sx={{
+              width: { xs: 150, md: 100 },
+              height: { xs: 150, md: 100 },
+            }}
+            image={`${api_base_url}${product.image_url}`}
+          />
+        </Paper>
+        <Box
+          sx={{
+            ml: 3,
+            display: "flex",
+            flexDirection: "column",
+            width: { md: 400 },
+          }}
+        >
+          <Box sx={{ maxWidth: { xs: 350, md: 500 } }}>
+            <Typography fontWeight="bold">{product.name}</Typography>
+          </Box>
+          <Box>
+            <Typography variant="caption">
+              {product.category.toUpperCase()}
+            </Typography>
+          </Box>
+          <Box>
+            <Typography>{PHPPrice.format(product.price)}</Typography>
+          </Box>
         </Box>
       </Box>
-    </Box>
 
-    <Box sx={{ alignSelf: "center", ml: { md: 3 } }}>
-      <ButtonGroup variant="contained">
-        <Button>
-          <RemoveIcon />
-        </Button>
-        <Typography sx={{ m: 1.3 }}>1</Typography>
-        <Button>
-          <AddIcon />
-        </Button>
-        <Button>
-          <DeleteIcon sx={{ color: "red" }} />
-        </Button>
-      </ButtonGroup>
+      <Box sx={{ alignSelf: "center", ml: { md: 3 } }}>
+        <ButtonGroup variant="contained">
+          <Button onClick={decreaseQuantity}>
+            <RemoveIcon />
+          </Button>
+          <Typography sx={{ m: 1.3 }}>{quantity}</Typography>
+          <Button onClick={increaseQuantity}>
+            <AddIcon />
+          </Button>
+          <Button onClick={deleteCart}>
+            <DeleteIcon sx={{ color: "red" }} />
+          </Button>
+        </ButtonGroup>
+      </Box>
+      {product.category === "customizable" ? (
+        <Box
+          sx={{ alignSelf: "center", ml: { md: 3 }, mt: { xs: 3, md: 0 } }}
+        >
+          <Paper>
+            <CardMedia
+              sx={{
+                width: { xs: 125, md: 90 },
+                height: { xs: 125, md: 90 },
+              }}
+              image={`${api_base_url}${image_url}`}
+            />
+          </Paper>
+        </Box>
+      ) : (
+        <Box sx={{ ml: { md: 14 } }} />
+      )}
     </Box>
-    <Box sx={{ alignSelf: "center", ml: { md: 3 }, mt: { xs: 3, md: 0 } }}>
-      <Paper>
-        <CardMedia
-          sx={{
-            width: { xs: 125, md: 90 },
-            height: { xs: 125, md: 90 },
-          }}
-          image="/images/sample/product.jpg"
-        />
-      </Paper>
-    </Box>
-  </Box>
-);
+  );
+};
 
 function CartItems() {
+  const navigate = useNavigate();
+  const user = useSelector(userSelector);
+  const {
+    data: cart_items = [],
+    isLoading,
+    isSuccess,
+  } = useGetCartByUserQuery(user.id);
+
+  const [checkoutOrder] = useCheckoutOrderMutation();
+
+  const checkOutClicked = async () => {
+    const data = { user: user.id };
+    data.items = cart_items.map((item) => {
+      return { ...item, price: item.product.price };
+    });
+    data.total_quantity = cart_items.reduce((total, item) => {
+      return total + item.quantity;
+    }, 0);
+    data.total_price = cart_items.reduce((total, item) => {
+      return total + item.product.price * item.quantity;
+    }, 0);
+    console.log({ data });
+
+    await checkoutOrder(data)
+      .then((res) => {
+        console.log("Create Order successfully", res);
+        navigate("/user/orders");
+      })
+      .catch((err) => console.log(err));
+  };
+
+  let content;
+  if (isLoading) {
+    content = (
+      <Box
+        sx={{
+          display: "flex",
+          justifyContent: "center",
+          alignItems: "center",
+          height: "40vh",
+        }}
+      >
+        <CircularProgress size={150} />
+      </Box>
+    );
+  } else if (isSuccess) {
+    content = (
+      <Box>
+        <Typography variant="h2" textAlign="center" sx={{ mt: 3 }}>
+          Your Shopping Cart
+        </Typography>
+        <Box
+          sx={{
+            mt: 3,
+            display: "flex",
+            flexDirection: "column",
+            alignItems: "center",
+          }}
+        >
+          <Stack spacing={6}>
+            {cart_items.map((item, i) => (
+              <CartItem key={item.id} {...item} i={i} />
+            ))}
+          </Stack>
+        </Box>
+        <Box display="flex" flexDirection="column" alignItems="center">
+          <Box>
+            <Typography variant="h3" textAlign="center" sx={{ mt: 3 }}>
+              TOTAL
+            </Typography>
+            <Typography variant="h4" textAlign="center" sx={{ mt: 3 }}>
+              {PHPPrice.format(
+                cart_items.reduce(
+                  (total, item) =>
+                    total + item.product.price * item.quantity,
+                  0
+                )
+              )}
+            </Typography>
+            <Typography variant="body1" textAlign="center">
+              Free Shipping
+            </Typography>
+          </Box>
+          <Button
+            variant="contained"
+            sx={{ mt: 3, width: 150 }}
+            size="large"
+            onClick={checkOutClicked}
+          >
+            CHECKOUT
+          </Button>
+        </Box>
+      </Box>
+    );
+  }
+
   return (
     <Box>
       <Box
@@ -114,42 +253,7 @@ function CartItems() {
           },
         }}
       />
-      <Container sx={{ minHeight: "10vh" }}>
-        <Typography variant="h2" textAlign="center" sx={{ mt: 3 }}>
-          Your Shopping Cart
-        </Typography>
-        <Box
-          sx={{
-            mt: 3,
-            display: "flex",
-            flexDirection: "column",
-            alignItems: "center",
-          }}
-        >
-          <CartItem />
-          <CartItem />
-        </Box>
-        <Box display="flex" flexDirection="column" alignItems="center">
-          <Box>
-            <Typography variant="h3" textAlign="center" sx={{ mt: 3 }}>
-              TOTAL
-            </Typography>
-            <Typography variant="h4" textAlign="center" sx={{ mt: 3 }}>
-              ₱1000.00
-            </Typography>
-            <Typography variant="body1" textAlign="center">
-              Free Shipping
-            </Typography>
-          </Box>
-          <Button
-            variant="contained"
-            sx={{ mt: 3, width: 150 }}
-            size="large"
-          >
-            CHECKOUT
-          </Button>
-        </Box>
-      </Container>
+      <Container sx={{ minHeight: "20vh" }}>{content}</Container>
       <Footer />
     </Box>
   );
