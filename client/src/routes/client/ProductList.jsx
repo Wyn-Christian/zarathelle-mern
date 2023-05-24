9c1f9dad2f031f6d1ef3a6bacb4463bca02f5230
Wyn Chrsitian <wynchristian.rebanal@tup.edu.ph>
2023-05-24 15:02:07 +0800

    Data fetching in Product List

--- client/src/routes/client/ProductList.jsx
+++ client/src/routes/client/ProductList.jsx
@@ -13,62 +13,111 @@ import { Link, useParams } from "react-router-dom";
 import Footer from "../../components/Footer";
 import Grid from "@mui/material/Unstable_Grid2";
 import {
+  useAddToCartMutation,
+  useGetCartByUserQuery,
   useGetCollectionQuery,
   useGetProductsOfCollectionQuery,
 } from "../../features/apiSlice";
 import { api_base_url } from "../../app/base_url";
+import { useSelector } from "react-redux";
+import { userSelector } from "../../features/usersSlice";
+import { PHPPrice } from "../../app/priceFormatter";
 
-const ProductCard = ({ id, name, description, image_url }) => (
-  <Grid
-    xs={12}
-    sm={6}
-    md={4}
-    sx={{
-      display: "flex",
-      flexDirection: "column",
-      alignItems: "center",
-    }}
-  >
-    <Card
+const ProductCard = ({
+  id,
+  name,
+  description,
+  image_url,
+  category,
+  price,
+}) => {
+  const [addToCart] = useAddToCartMutation();
+  const user = useSelector(userSelector);
+
+  const addToCartClicked = async () => {
+    if (user.id) {
+      await addToCart({ user: user.id, product: id });
+    } else {
+      console.log("Please Log in first!");
+    }
+  };
+
+  let customButton;
+  if (category === "ready made") {
+    customButton = (
+      <Button
+        size="small"
+        variant="outlined"
+        sx={{
+          fontWeight: "bold",
+          color: "#e78686",
+          borderColor: "#e78686",
+          ml: 2,
+        }}
+        onClick={addToCartClicked}
+      >
+        Add To Cart
+      </Button>
+    );
+  } else {
+    customButton = (
+      <Button
+        size="small"
+        LinkComponent={Link}
+        to={`/product/${id}`}
+        variant="outlined"
+        sx={{
+          fontWeight: "bold",
+          color: "#e78686",
+          borderColor: "#e78686",
+          ml: 2,
+        }}
+      >
+        Customize
+      </Button>
+    );
+  }
+  return (
+    <Grid
+      xs={12}
+      sm={6}
+      md={4}
       sx={{
-        maxWidth: 400,
-        width: 290,
-        height: "100%",
         display: "flex",
         flexDirection: "column",
-        justifyContent: "space-between",
+        alignItems: "center",
       }}
     >
-      <CardActionArea LinkComponent={Link} to={`/product/${id}`}>
-        <CardMedia
-          sx={{ height: 300 }}
-          image={`${api_base_url}${image_url}`}
-        />
-        <CardContent sx={{ textAlign: "center" }}>
-          <Typography variant="h5">{name}</Typography>
-          <Typography variant="body2">{description}</Typography>
-        </CardContent>
-      </CardActionArea>
-      <CardActions sx={{ ml: 4, mr: 5 }}>
-        <Typography sx={{ flexGrow: 1 }} fontWeight="bold">
-          ₱100.00
-        </Typography>
-        <Button
-          size="small"
-          variant="outlined"
-          sx={{
-            fontWeight: "bold",
-            color: "#e78686",
-            borderColor: "#e78686",
-            ml: 2,
-          }}
-        >
-          Add To Cart
-        </Button>
-      </CardActions>
-    </Card>
-  </Grid>
-);
+      <Card
+        sx={{
+          maxWidth: 400,
+          width: 290,
+          height: "100%",
+          display: "flex",
+          flexDirection: "column",
+          justifyContent: "space-between",
+        }}
+      >
+        <CardActionArea LinkComponent={Link} to={`/product/${id}`}>
+          <CardMedia
+            sx={{ height: 300 }}
+            image={`${api_base_url}${image_url}`}
+          />
+          <CardContent sx={{ textAlign: "center" }}>
+            <Typography variant="h5">{name}</Typography>
+            <Typography variant="body2">{description}</Typography>
+          </CardContent>
+        </CardActionArea>
+        <CardActions sx={{ ml: 4, mr: 5 }}>
+          <Typography sx={{ flexGrow: 1 }} fontWeight="bold">
+            {PHPPrice.format(price)}
+          </Typography>
+          {customButton}
+        </CardActions>
+      </Card>
+    </Grid>
+  );
+};
 
 function ProductList() {
   const { id } = useParams();
@@ -80,8 +129,6 @@ function ProductList() {
       image: "",
       category: "",
     },
-    isFetching,
-    isSuccess,
   } = useGetCollectionQuery(id);
 
   const {
@@ -97,7 +144,7 @@ function ProductList() {
       <Box
         sx={{
           background: `url("/images/collections.png") center center / cover no-repeat`,
-          height: "60vh",
+          height: { xs: "90vh", md: "60vh" },
           position: "relative",
           display: "flex",
           alignItems: "center",
@@ -114,7 +161,7 @@ function ProductList() {
           },
         }}
       >
-        <Container sx={{ zIndex: 1 }}>
+        <Container sx={{ zIndex: 1, mt: 5 }}>
           <Box
             sx={{
               display: "flex",
@@ -126,15 +173,15 @@ function ProductList() {
             <Card
               elevation={3}
               sx={{
-                height: { xs: 300, sm: 300 },
-                width: { xs: 300, sm: 300 },
+                height: { xs: 200, sm: 300 },
+                width: { xs: 200, sm: 300 },
                 m: 1,
               }}
             >
               <CardMedia
-                image={`/images/sample/collection.jpg`}
+                image={`${api_base_url}${collection.image_url}`}
                 sx={{
-                  height: "95%",
+                  height: { xs: "93%", sm: "95%" },
 
                   m: 1,
                 }}
@@ -142,8 +189,11 @@ function ProductList() {
             </Card>
             <Box ml={3} mt={4}>
               <Typography variant="h4">{collection.title}</Typography>
+              <Typography variant="caption">
+                {collection.category.toUpperCase()}
+              </Typography>
               <Typography variant="body1">
-                Sample Collection Description
+                {collection.description}
               </Typography>
             </Box>
           </Box>
