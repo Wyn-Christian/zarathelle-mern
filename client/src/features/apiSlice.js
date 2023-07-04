717256b868837dbb900924fa4ca58ba4e7fdf4d4
Wyn Chrsitian <wynchristian.rebanal@tup.edu.ph>
2023-07-04 23:06:08 +0800

    Add order in tagtypes

--- client/src/features/apiSlice.js
+++ client/src/features/apiSlice.js
@@ -3,7 +3,7 @@ import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 export const apiSlice = createApi({
   reducerPath: "api",
   baseQuery: fetchBaseQuery({ baseUrl: "http://localhost:4000" }),
-  tagTypes: ["Collection", "Product", "Cart", "User"],
+  tagTypes: ["Collection", "Product", "Cart", "User", "Order"],
   endpoints: (builder) => ({
     // Collections endpoints
     getCollections: builder.query({
