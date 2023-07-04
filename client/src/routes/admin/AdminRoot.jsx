6b8fea71492b8c463ba527afc1416311b3d40cb2
Wyn Chrsitian <80365421+Wyn-Christian@users.noreply.github.com>
2023-07-04 23:51:51 +0800

    Add feature: admin authentication

--- client/src/routes/admin/AdminRoot.jsx
+++ client/src/routes/admin/AdminRoot.jsx
@@ -1,26 +1,36 @@
 import { Box, Toolbar } from "@mui/material";
-import { Outlet } from "react-router-dom";
+import { Outlet, useNavigate } from "react-router-dom";
 import AdminNavBar from "../../components/AdminNavbar";
+import { useSelector } from "react-redux";
+import { userSelector } from "../../features/usersSlice";
+import { useEffect } from "react";
 
 const drawerWidth = 240;
 
 function AdminRoot() {
-  return (
-    <Box display="flex">
-      <AdminNavBar />
-      <Box
-        component="main"
-        sx={{
-          flexGrow: 1,
-          p: 3,
-          width: { xs: "100%", sm: `calc(100% - ${drawerWidth}px)` },
-        }}
-      >
-        <Toolbar />
-        <Outlet />
-      </Box>
-    </Box>
-  );
+	const navigate = useNavigate();
+	const user = useSelector(userSelector);
+	useEffect(() => {
+		if (user.position !== "admin") {
+			navigate("/");
+		}
+	}, [user]);
+	return (
+		<Box display="flex">
+			<AdminNavBar />
+			<Box
+				component="main"
+				sx={{
+					flexGrow: 1,
+					p: 3,
+					width: { xs: "100%", sm: `calc(100% - ${drawerWidth}px)` },
+				}}
+			>
+				<Toolbar />
+				<Outlet />
+			</Box>
+		</Box>
+	);
 }
 
 export default AdminRoot;
